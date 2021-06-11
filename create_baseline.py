c2582c9820bef6d362f62becb1806ee9604eea29
Gautam J <gautam.jayapal@gmail.com>
2021-06-11 12:21:14 +0530

    update create_baseline.py

--- create_baseline.py
+++ create_baseline.py
@@ -4,7 +4,7 @@ from tensorflow.keras.datasets import cifar10
 from utils.data import set_random_seed, split_data, normalize_image
 from utils.analysis import setup_logger, get_error_pct, get_f1_score
 
-logger = setup_logger('baseline_logger', log_file='logs/baselinelogs.csv')
+logger = setup_logger('baseline_logger', log_file='logs/baselinelogs.txt')
 logger.info('=' * 50)
 
 set_random_seed()
@@ -31,10 +31,18 @@ logger.debug(f'{y_test.shape = }')
 model = DummyClassifier()
 model.fit(x_train, y_train)
 
-dummy_val_error_pct = get_error_pct(model, x_val, y_val) * 100
-logger.critical(f'{dummy_val_error_pct = :.4f} %')
-
+dummy_train_f1_score = get_f1_score(model, x_train, y_train)
+logger.critical(f'{dummy_train_f1_score = :.4f}')
 dummy_val_f1_score = get_f1_score(model, x_val, y_val)
 logger.critical(f'{dummy_val_f1_score = :.4f}')
+dummy_test_f1_score = get_f1_score(model, x_test, y_test)
+logger.critical(f'{dummy_test_f1_score = :.4f}')
+
+dummy_train_error_pct = get_error_pct(model, x_train, y_train) * 100
+logger.critical(f'{dummy_train_error_pct = :.4f} %')
+dummy_val_error_pct = get_error_pct(model, x_val, y_val) * 100
+logger.critical(f'{dummy_val_error_pct = :.4f} %')
+dummy_test_error_pct = get_error_pct(model, x_test, y_test) * 100
+logger.critical(f'{dummy_test_error_pct = :.4f} %')
 
 logger.info('=' * 50)
